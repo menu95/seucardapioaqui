d209d08b231b46c9fd06fa164475450b9444848d
menu95 <100180464+menu95@users.noreply.github.com>
2022-02-22 12:17:50 -0300

    version 0.1

---
+++ menu.js
@@ -0,0 +1,20 @@
+let menuJson = [
+    {id:1, name:'BIG-FOOD R$ 9,90', img:'image/bigmac.jpeg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'pão com gergelim, 2 carnes, alface, tomate e cebola e molho especial'},
+    {id:2, name:'FABULOSO R$ 9,90', img:'image/Burger.jpeg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'pão com gergelim, carne grelhada, maionese especial, alface, tomate, cebola picles'},
+    {id:3, name:'TRIPLO QUEIJO R$ 12,00', img:'image/triploxburguer.jpg', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'3 Carnes, queijo 3 vezes, pão, molho especial, picles, tomate e cebola '},
+    {id:4, name:'X-BURGUER R$ 8,00', img:'image/xduplo.jpg', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'pão com gergelçim, queijo em dobro, tomate, cebola, alface, picles '},
+    {id:5, name:'CHEDDAR DUPLO R$ 8,90', img:'image/cheddarduplo.jpeg', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'pão com gergelim, 2 caenes, cheddar em dobro, cebola caramelizada'},
+    {id:6, name:'CHICKEN BURGUER R$ 11,80', img:'image/hfrango.jpeg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'pão com gergelim, molho especial, chicken empanado, alface, tomate, cebola '},
+    {id:7, name:'CATUPIRY 4 CARNES R$ 18,90', img:'image/catupiry.jpg', price:[18.90, 18.90, 18.90], sizes:[' ', '',''], description:'pão com gergelim, 4 carnes de hamburguer, catupiry, bacon e molho especial'},
+    {id:8, name:'PIZZA DE PEPERONE R$ 38,00', img:'image/novapizza.jpg', price:[38.00, 38.00, 38.00], sizes:['','', ''], description:'Pizza de peperone: massa a base de trigo, molho de tomate, queijo mussarela, deliciosas fatias de peperone'},
+    {id:9, name:'AGUA MINERAL R$2,00', img:'image/agua.jpeg', price:[2.00, 2.00, 2.00], sizes:['','', ''], description:'agua mineral 500ml'},
+    {id:10, name:'HAMBURGUER R$ 7,90 ', img:'image/xburguercomum.jpg', price:[7.90, 7.90, 7.90], sizes:[' ', '',''], description:'pão, 1 carne de hamburguer, queijo, picles, tomate, cebola e molho especial'},
+    {id:11, name:'ONIONS BURGUER R$ 10,90', img:'image/onionsburguer.png', price:[10.90, 10.90, 10.90], sizes:[' ', '',''], description:'pão, 2 carnes, onions empanadas, queijo , bacon'},
+    {id:12, name:'TRIPLO BURGUER R$ 12,00', img:'image/triploxburguer.jpg', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'3 Carnes, queijo 3 vezes, pão, molho especial, picles, tomate e cebola '},
+ 
+   
+];
+
+
+
+
